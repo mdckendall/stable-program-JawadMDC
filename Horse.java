749212fd225c3d69c115a63e040ac14acf3038f3
Jawad Mohammed <113356691+JawadMDC@users.noreply.github.com>
2022-10-06 10:15:28 -0400

    Update Horse.java

--- Horse.java
+++ Horse.java
@@ -1,25 +1,30 @@
 //Create the Horse class here.
 
 public class Horse {
-    public String name_horse;
-    public double weight_horse;
-    public boolean tame_horse;
+    private String name_horse;
+    private double weight_horse;
+    private boolean tame_horse;
 
 public Horse(String name_horse, double weight_horse, boolean tame_horse)
   {
     this.name_horse = name_horse;
     this.weight_horse = weight_horse;
-    this.tame_horse = tame_horse;
-    
+    this.tame_horse = tame_horse;   
   }
   
-   public String getName(){
+   public String getName()
+    {
         return this.name_horse;
     }
-    public double getWeight(){
+    
+    public double getWeight()
+    {
         return this.weight_horse;
     }
-    public boolean getTame(){
+    
+    public boolean getTame()
+    {
         return this.tame_horse;
     }
+    
 }
