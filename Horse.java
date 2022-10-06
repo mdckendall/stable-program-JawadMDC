ae87d41fa791fbcaae1d95fbaf22ebe19963ba57
Jawad Mohammed <113356691+JawadMDC@users.noreply.github.com>
2022-10-06 09:35:44 -0400

    Update Horse.java

--- Horse.java
+++ Horse.java
@@ -1,1 +1,16 @@
 //Create the Horse class here.
+
+public class Horse {
+    String name_horse;
+    double weight_horse;
+    boolean tame_horse;
+
+public Horse(String name_horse, double weight_horse, boolean tame_horse)
+  {
+    this.name_horse = name_horse;
+    this.weight_horse = weight_horse;
+    this.tame_horse = tame_horse;
+    
+  }
+  
+}
